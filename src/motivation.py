9f0fb3c32f78250d3b0f83637ce1b9d493a3a9f2
joakimOL <joakim.o.l@hotmail.com>
2019-09-15 15:48:03 +0200

    initial commit

---
+++ src/motivation.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import random
+import subprocess
+import argparse
+
+def polybar(args):
+    thresholds = [25,20,30]
+    with open ("/home/joakim/Documents/Python/Motivation/quotes.txt","r") as f:
+        f = f.readlines()
+        quote = random.choice(f).strip()
+        colors = [hex(random.randint(0,2**8)).strip("0x").zfill(2) for i in range(3)]
+        colorstring = "#" + colors[0]+ colors[1]+ colors[2]
+
+        cmdlist = ["notify-send", quote, "-i", "/home/joakim/Documents/Python/Motivation/harvåd.png"]
+        process = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
+
+        _, _ = process.communicate()
+        print(format("%%{F%s}%s"
+            %(colorstring, quote)))
+
+def xmobar(args):
+    thresholds = [25,20,30]
+    with open ("/home/joakim/Documents/Python/Motivation/quotes.txt","r") as f:
+        f = f.readlines()
+        quote = random.choice(f).strip()
+        colors = [hex(random.randint(thresholds[i],2**8)).strip("0x").zfill(2) for i in range(3)]
+        colorstring = "#" + colors[0]+ colors[1]+ colors[2]
+
+        cmdlist = ["notify-send", quote, "-i", "/home/joakim/Documents/Python/Motivation/harvåd.png"]
+        process = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
+
+        _, _ = process.communicate()
+        print(format("<fc=%s>%s</fc>"
+            %(colorstring, quote)))
+
+def windows(args):
+    print("fuck you, not implemented yet")
+
+
+if __name__ == "__main__":
+    parser =  argparse.ArgumentParser(description='what is this')
+    parser.add_argument('--mode', help='polybar/xmobar', type=str, choices=['polybar','xmobar',
+        'windows'], default='xmobar')
+    args = parser.parse_args()
+
+    # print(args.mode)
+    if args.mode == "polybar":
+        polybar(args)
+    elif args.mode == "xmobar":
+        xmobar(args)
+    elif args.mode == "windows":
+        windows(args)
+    else:
+        print("please specify which bar to use")
+
