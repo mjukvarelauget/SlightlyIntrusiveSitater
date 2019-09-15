707fd74ce45ed1db762944c72f85a68954999aa0
joakimOL <joakim.o.l@hotmail.com>
2019-09-15 18:05:21 +0200

    Refactor 🕋

--- src/motivation.py
+++ src/motivation.py
@@ -8,6 +8,17 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+format_strings = {
+        "polybar"  : "%%{F%s}%s",
+        "xmobar"   : "<fc=%s>%s</fc>",
+        "lemonbar" : "%s%s",
+        "windows"  : "%s%s",
+        "i3bar"    : "%s%s"
+    }
+
+thresholds = [25,20,30]
+bars = ["polybar","xmobar","windows","lemonbar","i3bar"]
+
 def notify(quote, image_path = None):
     cmdlist = ["notify-send", quote, "-i", image_path]
     if(image_path == None):
@@ -17,9 +28,14 @@ def notify(quote, image_path = None):
     _, _ = process.communicate()
 
 
-def motivate(format_string, image_path):
-    thresholds = [25,20,30]
-    with open(os.path.join(dir_path, "..","data","quotes.txt")) as f:
+def motivate(format_string, image_path = None, list_path = None):
+    if(image_path == None):
+        image_path = os.path.join(dir_path, "..","assets","harvåd.png")
+
+    if(list_path == None):
+        list_path = os.path.join(dir_path, "..","data","quotes.txt")
+
+    with open(list_path) as f:
         f = f.readlines()
         quote = random.choice(f).strip()
         colors = [hex(random.randint(thresholds[i],2**8)).strip("0x").zfill(2) for i in range(3)]
@@ -30,21 +46,10 @@ def motivate(format_string, image_path):
         print(format(format_string
             %(colorstring, quote)))
 
-def polybar(args):
-    motivate("%%{F%s}%s", os.path.join(dir_path, "..","assets","harvåd.png"))
-
-def xmobar(args):
-    motivate("<fc=%s>%s</fc>", os.path.join(dir_path, "..","assets","harvåd.png"))
-
-def windows(args):
-    print("fuck you, not implemented yet")
-
 
 if __name__ == "__main__":
-
-    bars = ["polybar","xmobar","windows","lemonbar","i3bar"]
     parser =  argparse.ArgumentParser(description='what is this')
-    parser.add_argument('--mode', help='polybar/xmobar/windows/lemonbar/i3bar', type=str, choices=bars)
+    parser.add_argument('--mode', help='/'.join(bars), type=str, choices=bars)
     parser.add_argument('-i', '--image-path', help='notification icon path', type=str)
     parser.add_argument('-l', '--list', help='quote list file path', type=str)
     args = parser.parse_args()
@@ -53,13 +58,9 @@ if __name__ == "__main__":
         parent = psutil.Process(psutil.Process().ppid())
         if parent.name() in bars:
             args.mode = parent.name()
+        else:
+            print("Could not determine which bar you're using and no bar was specified using --mode\nAborting")
+            exit(1)
 
-    if args.mode == "polybar":
-        polybar(args)
-    elif args.mode == "xmobar":
-        xmobar(args)
-    elif args.mode == "windows":
-        windows(args)
-    else:
-        print("Could not determine which bar you're using and no bar was specified using --mode\nAborting")
 
+    motivate(format_strings[args.mode])
