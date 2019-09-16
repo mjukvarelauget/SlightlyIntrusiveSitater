385a76a7f48d0bf74fe13a547bf07efedacdd7b9
JoakimOL <joakim.o.l@hotmail.com>
2019-09-16 16:43:21 +0200

    Implement (#1) customize colors
    Adds two flags, one to disable colors entirely and one to set a custom color.

--- src/motivation.py
+++ src/motivation.py
@@ -13,7 +13,8 @@ format_strings = {
         "xmobar"   : "<fc=%s>%s</fc>",
         "lemonbar" : "%s%s",
         "windows"  : "%s%s",
-        "i3bar"    : "%s%s"
+        "i3bar"    : "%s%s",
+        "default"  : "%s%s"
     }
 
 thresholds = [25,20,30]
@@ -28,8 +29,19 @@ def notify(quote, image_path = None):
     process = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
     _, _ = process.communicate()
 
+def determine_colors(color):
+    if(color == None):
+        colors = [hex(random.randint(thresholds[i],2**8)).strip("0x").zfill(2) for i in range(3)]
+        return '#' + colors[0] + colors[1] + colors[2]
+    else:
+        color = color.split("0x")[-1]
+        if(len(color) != 6):
+            print("Wrong color format!\nAborting")
+            exit(3)
+        return '#' + color
 
-def motivate(format_string, image_path = None, list_path = None):
+
+def motivate(format_string, color = None, no_color = False, image_path = None, list_path = None):
     if(image_path == None):
         image_path = os.path.join(dir_path, "..","assets","harvåd.png")
     elif(not os.path.isabs(image_path)):
@@ -38,12 +50,14 @@ def motivate(format_string, image_path = None, list_path = None):
     if(list_path == None):
         list_path = os.path.join(dir_path, "..","data","quotes.txt")
 
+    if(no_color):
+        colorstring=''
+    else:
+        colorstring = determine_colors(color)
+
     with open(list_path) as f:
         f = f.readlines()
         quote = random.choice(f).strip()
-        colors = [hex(random.randint(thresholds[i],2**8)).strip("0x").zfill(2) for i in range(3)]
-        colorstring = "#" + colors[0] + colors[1] + colors[2]
-
         notify(quote, image_path)
 
         print(format(format_string
@@ -55,6 +69,11 @@ if __name__ == "__main__":
     parser.add_argument('--mode', help='/'.join(bars), type=str, choices=bars)
     parser.add_argument('-i', '--image-path', help='notification icon path', type=str)
     parser.add_argument('-l', '--list', help='quote list file path', type=str)
+
+    color_group = parser.add_mutually_exclusive_group()
+    color_group.add_argument('-c', '--color', help='text color', type=str)
+    color_group.add_argument('-n', '--no-color', help='no text coloring', action='store_true', default=False)
+
     args = parser.parse_args()
 
     if(args.mode == None):
@@ -66,6 +85,8 @@ if __name__ == "__main__":
             exit(1)
 
     motivate(
-            format_string = format_strings[args.mode],
+            format_string = format_strings["default" if args.no_color else args.mode],
+            color         = args.color,
+            no_color      = args.no_color,
             image_path    = args.image_path,
             list_path     = args.list)
